79a1b45c8bd87507769e8fdf745ed980fd1a85c0
Лилия <zainetdinovalg@mail.ru>
2018-05-02 23:21:04 +0400

    Добавила комментарий

--- src/main/java/ui/MainFrame.java
+++ src/main/java/ui/MainFrame.java
@@ -33,7 +33,7 @@ public class MainFrame extends JFrame {
                 calculate(file);
             }
         });
-        panel.add(maxNumLabel);
+        panel.add(maxNumLabel);//
         panel.add(minNumLabel);
         panel.add(countLabel);
         panel.add(avgLabel);
